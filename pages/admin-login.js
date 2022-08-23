8bf8de597fad450361894a4a7496596cf041e6f9
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2022-08-23 13:30:03 +0600

    Added admin login and admin-panel navbar and sidebar

---
+++ pages/admin-login.js
@@ -0,0 +1,82 @@
+import { Link } from 'next/link';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Divider, Grid, Stack, Typography, useMediaQuery } from '@mui/material';
+
+// project imports
+import AuthWrapper from './components/Auth/AuthWrapper';
+import AuthCardWrapper from './components/Auth/AuthCardWrapper';
+import AuthLogin from './components/Auth/AuthLogin';
+import Logo from './components/Home/ui-component/Logo';
+import AuthFooter from './components/Home/ui-component/cards/AuthFooter';
+
+// assets
+
+// ================================|| AUTH3 - LOGIN ||================================ //
+
+const Login = () => {
+    const theme = useTheme();
+    const matchDownSM = useMediaQuery(theme.breakpoints.down('md'));
+
+    return (
+        <AuthWrapper>
+            <Grid container direction="column" justifyContent="flex-end" sx={{ minHeight: '100vh' }}>
+                <Grid item xs={12}>
+                    <Grid container justifyContent="center" alignItems="center" sx={{ minHeight: 'calc(100vh - 68px)' }}>
+                        <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+                            <AuthCardWrapper>
+                                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                                    <Grid item sx={{ mb: 3 }}>
+                                        
+                                            {/* <Logo /> */}
+                                        
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Grid
+                                            container
+                                            direction={matchDownSM ? 'column-reverse' : 'row'}
+                                            alignItems="center"
+                                            justifyContent="center"
+                                        >
+                                            <Grid item>
+                                                <Stack alignItems="center" justifyContent="center" spacing={1}>
+                                                    <Typography
+                                                        color={theme.palette.secondary.main}
+                                                        gutterBottom
+                                                        variant={matchDownSM ? 'h3' : 'h2'}
+                                                    >
+                                                        Admin Login
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="caption"
+                                                        fontSize="16px"
+                                                        textAlign={matchDownSM ? 'center' : 'inherit'}
+                                                    >
+                                                        Enter your credentials to continue
+                                                    </Typography>
+                                                </Stack>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <AuthLogin />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Divider />
+                                    </Grid>
+
+                                </Grid>
+                            </AuthCardWrapper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
+                    <AuthFooter />
+                </Grid>
+            </Grid>
+        </AuthWrapper>
+    );
+};
+
+export default Login;
