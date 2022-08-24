03354c28e5b8c037cc883abbf86e105612f8df13
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2022-08-24 20:28:48 +0600

    Updated Image page

--- pages/images.js
+++ pages/images.js
@@ -11,6 +11,7 @@ const imageSrc = require("../pages/assets/images/species1.jpg");
 import { useRouter } from "next/router";
 import Header from "./components/Home/Header";
 import { styled } from "@mui/material/styles";
+import { speciesList } from "./utils/speciesList";
 import {
   AppBar,
   Box,
@@ -27,10 +28,14 @@ import {
   TableHead,
   Table,
   TableContainer,
-  Button,Paper,
+  Button,
+  Paper,
   Modal,
+  Card,
+  CardActions,
   CardContent,
   tableCellClasses,
+  Divider,
 } from "@mui/material";
 import Footer from "./components/Home/Footer/Footer";
 import Counters from "./components/Home/counters";
@@ -49,20 +54,20 @@ function createData(
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
+    color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14
-  }
+    fontSize: 14,
+  },
 }));
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover
+    backgroundColor: theme.palette.action.hover,
   },
   // hide last border
   "&:last-child td, &:last-child th": {
-    border: 0
-  }
+    border: 0,
+  },
 }));
 const style = {
   position: "absolute",
@@ -75,56 +80,6 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
-const rows = [
-  createData(
-    1,
-    "Bryopsis indica Gepp & Gepp",
-    "Bryopsidaceae",
-    "St Martin’s Island (SMI)",
-    "rocks, corals",
-    "2-3",
-    "20.622990,92.320325"
-  ),
-  createData(
-    2,
-    "Bryopsis indica Gepp & Gepp",
-    "Bryopsidaceae",
-    "St Martin’s Island (SMI)",
-    "rocks, corals",
-    "2-3",
-    "20.622990,92.320325"
-  ),
-
-  createData(
-    3,
-    "Bryopsis indica Gepp & Gepp",
-    "Bryopsidaceae",
-    "St Martin’s Island (SMI)",
-    "rocks, corals",
-    "2-3",
-    "20.622990,92.320325"
-  ),
-
-  createData(
-    4,
-    "Bryopsis indica Gepp & Gepp",
-    "Bryopsidaceae",
-    "St Martin’s Island (SMI)",
-    "rocks, corals",
-    "2-3",
-    "20.622990,92.320325"
-  ),
-
-  createData(
-    5,
-    "Bryopsis indica Gepp & Gepp",
-    "Bryopsidaceae",
-    "St Martin’s Island (SMI)",
-    "rocks, corals",
-    "2-3",
-    "20.622990,92.320325"
-  ),
-];
 function srcset(image, width, height, rows = 1, cols = 1) {
   return {
     src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
@@ -141,21 +96,22 @@ export default function Images() {
   const router = useRouter();
   return (
     <div className={styles.container}>
-
       <Header index={2}></Header>
-      <div className={styles.main_box}  >
-        <Box component="section" sx={{ mt: 10,}}>
-
-        <Grid container item xs={12} md={12} sx={{ mx: "auto" }}>
-            <Grid item xs={12} md={8}>
-            <Typography gutterBottom component="h2" variant="h2">
-                  Species Search
-                </Typography>
-                <Typography gutterBottom component="description" variant="div">
-                The images remain the property of the copyright owners who give permission for non-commercial use for teaching purposes in lectures and on meetings presentations and posters, provided their 
-                copyright and the source is acknowledged, but are NOT free for publication in any format or manner.
-                </Typography>
-              <Grid item xs={12}>
+      <div className={styles.main_box}>
+        <Box component="section" sx={{ mt: 10 }}>
+          <Grid container item xs={12} md={12} sx={{ mx: "auto" }}>
+            <Grid item xs={12} md={12}>
+              <Typography gutterBottom component="h2" variant="h2">
+                Images Search
+              </Typography>
+              <Typography gutterBottom component="description" variant="div">
+                The images remain the property of the copyright owners who give
+                permission for non-commercial use for teaching purposes in
+                lectures and on meetings presentations and posters, provided
+                their copyright and the source is acknowledged, but are NOT free
+                for publication in any format or manner.
+              </Typography>
+              <Grid item xs={12} sx={{ mb: 2 }}>
                 <Box
                   component="form"
                   sx={{
@@ -164,184 +120,61 @@ export default function Images() {
                   noValidate
                   autoComplete="off"
                 >
-                  <TextField label="Search By Name" color="secondary"  />
-                  <TextField label="Search By  Family" color="secondary"  />
-                  <TextField label="Select Country" color="secondary"  />
-                  <TextField label="Select Area" color="secondary"  />
+                  <TextField label="Search By Name" color="secondary" />
+                  <TextField label="Search By  Family" color="secondary" />
+                  <TextField label="Select Country" color="secondary" />
+                  <TextField label="Select Area" color="secondary" />
                   <Button
                     type="button"
                     // onClick={}
-                    style={{color: "white" , background:"purple" ,          maxWidth: "80px",
-                    maxHeight: "80px",
-                    minWidth: "40px",
-                    minHeight: "40px"
-           }}
+                    style={{
+                      color: "white",
+                      background: "purple",
+                      maxWidth: "80px",
+                      maxHeight: "80px",
+                      minWidth: "40px",
+                      minHeight: "40px",
+                    }}
                   >
                     Search
                   </Button>
                 </Box>
               </Grid>
-                    
-              <h1>Table</h1>
-              <Grid item xs={12}  sx={{boxShadow: 4,p:4}}    style={{ borderRadius: "10px", }} >
-                <TableContainer component={Paper}    >
-                  <Table sx={{ minWidth: 650 }} aria-label="customized table" >
-                    <TableHead>
-                      <TableRow>
-                        <StyledTableCell>SI</StyledTableCell>
-                        <StyledTableCell align="center">Species</StyledTableCell>
-                        <StyledTableCell align="center">Family</StyledTableCell>
-                        <StyledTableCell align="center">Locality</StyledTableCell>
-                        <StyledTableCell align="center">Habitat</StyledTableCell>
-                        <StyledTableCell align="center">Size &nbsp;(cm)</StyledTableCell>
-                        <StyledTableCell align="center">GIS</StyledTableCell>
-                        <StyledTableCell align="center">Additional button</StyledTableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody   >
-                      {rows.map((row) => (
-                        <StyledTableRow
-                          key={row.name}
-                          sx={{
-                            "&:last-child td, &:last-child th": { border: 0 },
-                          }}
-                        
-                        >
-                          <StyledTableCell component="th" scope="row">
-                            {row.number}
-                          </StyledTableCell>
-                          <StyledTableCell align="center">{row.Species}</StyledTableCell>
-                          <StyledTableCell align="center">{row.Family}</StyledTableCell>
-                          <StyledTableCell align="center">{row.Locality}</StyledTableCell>
-                          <StyledTableCell align="center">{row.Habitat}</StyledTableCell>
-                          <StyledTableCell align="center">{row.Size}</StyledTableCell>
-                          <StyledTableCell align="center">{row.GIS}</StyledTableCell>
-                          <StyledTableCell align="center">
-                            <Button style={{    maxWidth: "80px",
-                             maxHeight: "80px",
-                             minWidth: "40px",
-                             minHeight: "40px"}} onClick={handleOpen} sx={{ mb: 1, mr:0.5 }} variant="outlined"  >
-                              Details
-                            </Button>
-
-                            {/* =======MODAL===== */}
-
-                            <Modal
-                              open={open}
-                              onClose={handleClose}
-                              aria-labelledby="modal-modal-title"
-                              aria-describedby="modal-modal-description"
-                            >
-                              <Box sx={style} className="modal-size">
-                            
-                              <Grid sx={{ maxWidth: 345 }}    >    <Image src={imageSrc} 
-                                    // width={500}
-                                    // height={300}
-                                  
-                                  ></Image></Grid>
-                              
-                                <CardContent>
-        <Typography gutterBottom variant = "h5" component = "div">
-          Lizard
-        </Typography>
-        <Typography variant   = "body2" color  = "text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-                                {/* <CardActions>
-                                <Button size="small">Share</Button>
-                                <Button size="small">Learn More</Button>
-                               </CardActions> */}
-                              </Box>
-                            </Modal>
-                            <br />
-                            <Button
-                             style={{ maxWidth: "80px",
-                             maxHeight: "80px",
-                             minWidth: "40px",
-                             minHeight: "40px"
-                    }}
-                              type="button"
-                              onClick={() => router.push("/map")}
-                              variant="outlined"
-                            >
-                              View&nbsp;map
-                            </Button>
-                          </StyledTableCell>
-                        </StyledTableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-                {/* <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      /> */}
+              <Divider></Divider>
+              <Grid container xs={12} sx={{ mt: 5, mb: 0 }}>
+                {speciesList?.map((item) => {
+                  return (<Grid item xs={6} md={3} sx={{mb: 3}}>
+                    <Card
+                      sx={{
+                        maxWidth: 345,
+                        border: "1px solid #e9e9e9",
+                        boxShadow: "1px 1px 5px #efefef",
+                      }}
+                    >
+                      <Image
+                        component="img"
+                        height="400"
+                        src={imageSrc}
+                        alt="green iguana"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h4" component="div">
+                          {item.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                         {item.description}
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button size="small">See Details</Button>
+                      </CardActions>
+                    </Card>
+                  </Grid>)
+                })}
               </Grid>
 
-              <Grid item xs={12} padding="30">
-              <ImageList
-            sx={{
-              width:"50%",
-              mx:"auto",
-              // Promote the list into its own layer in Chrome. This costs memory, but helps keeping high FPS.
-              transform: "translateZ(0)",
-            }}
-            rowHeight={200}
-            gap={1}
-          >
-            {itemData.map((item) => {
-              const cols = 1;
-              const rows = 1;
-
-              return (
-                <ImageListItem key={item.img} cols={cols} rows={rows}>
-                  <img
-                    {...srcset(item.img, 250, 200, rows, cols)}
-                    alt={item.title}
-                    loading="lazy"
-                  />
-                  <ImageListItemBar
-                    sx={{
-                      background:
-                        "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
-                        "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
-                    }}
-                    title={item.title}
-                    position="top"
-                    actionIcon={
-                      <IconButton
-                        sx={{ color: "white" }}
-                        aria-label={`star ${item.title}`}
-                      >
-                        <StarBorderIcon />
-                      </IconButton>
-                    }
-                    actionPosition="left"
-                  />
-                </ImageListItem>
-              );
-            })}
-          </ImageList>
-              </Grid>
-              <Counters ></Counters>
-            </Grid>
-            <Grid  item
-              xs={12}
-              md={3}
-              className={styles.side_bar}
-              m={2}
-              sx={{boxShadow: 4,p:4}} >
-              <Typography gutterBottom variant="h2" component="div">
-                Latest Additions
-              </Typography>
-              <CollapseCard />
+              <Grid item xs={12} padding="30"></Grid>
+              {/* <Counters></Counters> */}
             </Grid>
           </Grid>
           {/* <ImageList
@@ -387,11 +220,10 @@ export default function Images() {
               );
             })}
           </ImageList> */}
-          
         </Box>
       </div>
       <footer className={styles.footer}>
-     <Footer/>
+        <Footer />
       </footer>
     </div>
   );
