538616f0b62c8af7600d4ff9f0ff04452a2eb86b
yeatun <p.yeatun@gmail.com>
2022-09-07 13:56:52 +0600

    update home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import * as React from 'react';
+import * as React from "react";
 import Head from "next/head";
 import Image from "next/image";
+import { speciesList } from "./utils/speciesList";
 import {
   AppBar,
   Box,
@@ -19,6 +20,12 @@ import {
   Paper,
   Input,
   OutlinedInput,
+  CardContent,
+  Card,
+  ImageList,
+  ImageListItem,
+  ListSubheader,
+  ImageListItemBar,
 } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
 import SearchIcon from "@mui/icons-material/Search";
@@ -30,8 +37,10 @@ import CuroselCard from "./components/Home/curoselCard";
 import CollapseCard from "./components/Home/collapseCard";
 import Footer from "./components/Home/Footer/Footer";
 const img = require("../pages/assets/images/species3.jpg");
+const species2 = require("../pages/assets/images/species2.jpg");
 import { Icon } from "@iconify/react";
 import { height, width } from "@mui/system";
+import InfoIcon from "@mui/icons-material/Info";
 
 export default function Home() {
   const [spacing, setSpacing] = React.useState(2);
@@ -91,7 +100,10 @@ export default function Home() {
                       // pr: "25px",
                     }}
                   >
-                    <FormControl sx={{ width: "25ch" }}    className={styles.search}>
+                    <FormControl
+                      sx={{ width: "25ch" }}
+                      className={styles.search}
+                    >
                       <OutlinedInput placeholder="Please enter text" />
                     </FormControl>
                     <Button
@@ -113,43 +125,63 @@ export default function Home() {
         <DirectionsIcon />
       </IconButton> */}
                   </Grid>
-                  
                 </Grid>
               </Grid>
 
               {/* <h1 className={styles.title}>Getting started BIO-DIVERSITY!</h1> */}
             </Grid>
-       
           </Grid>
-           <Grid className={styles.secondContainer}>
-            
+          <Grid className={styles.secondContainer}>
+            <Grid
+              className={styles.feature}
+              container
+              spacing={2}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
 
-          <Grid className="col-md-12">
-					   <h3 role="heading" aria-level="2" id="recent-sightings">Recent sightings</h3>
-				   </Grid>
-           
-           <Grid sx={{ flexGrow: 1 }} container spacing={1}>
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={spacing}>
-          {[0, 1, 2].map((value) => (
-            <Grid key={value} item>
-              <Paper
-                sx={{
-                  height: 200,
-                  width: 200,
-                  backgroundColor: (theme) =>
-                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-                }}
-              />
+              // style={{ minHeight: "100vh" }}
+            >
+              <Typography
+                paddingRight={107}
+                paddingBottom={3}
+                fontSize={30}
+                fontWeight={700}
+              >
+                Recent sightings
+              </Typography>
+
+              <ImageList
+                gap={40}
+                sx={{ width: 1100, height:550 }}
+                cols={3}
+                rowHeight={350}
+
+              >
+                {itemData.map((item) => (
+                  <ImageListItem key={item.img}>
+                    <img
+                      src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                      srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                      alt={item.title}
+                      loading="lazy"
+                    />
+                    <ImageListItemBar
+                      title={item.title}
+                      subtitle={item.author}
+                      position="bottom"
+                      
+                      sx={{
+                        background:
+                          'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+                          'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+                      }}
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
             </Grid>
-          ))}
-        </Grid>
-      </Grid>
-      {/* <Grid item xs={12}>
-
-      </Grid> */}
-    </Grid>
-       </Grid>
+          </Grid>
         </Box>
       </div>
 
@@ -157,3 +189,21 @@ export default function Home() {
     </div>
   );
 }
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "@bkristastucchio",
+    featured: true,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+    author: "@rollelflex_graphy726",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+    author: "@helloimnik",
+  },
+];
