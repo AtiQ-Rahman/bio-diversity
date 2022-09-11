a75dc087328e98edf8a49ea9f38d50f3351e64a6
yeatun <p.yeatun@gmail.com>
2022-09-11 16:06:28 +0600

    update admin dashboard

--- pages/admin-dashboard.js
+++ pages/admin-dashboard.js
@@ -4,107 +4,189 @@ import {
   AppBar,
   Box,
   CssBaseline,
+  Grid,
   Toolbar,
   useMediaQuery,
 } from "@mui/material";
 
 import { styled, useTheme } from "@mui/material/styles";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Header from "./components/Admin/Header";
-import Sidebar from './components/Admin/Sidebar';
-import Breadcrumbs from './components/Home/ui-component/extended/Breadcrumbs';
-import { useDispatch, useSelector } from 'react-redux';
-import { IconChevronRight } from '@tabler/icons';
-import navigation from './components/Admin/menu-items';
-import { drawerWidth } from './store/constant';
-import { SET_MENU } from './store/actions';
+import Sidebar from "./components/Admin/Sidebar";
+import Breadcrumbs from "./components/Home/ui-component/extended/Breadcrumbs";
+import { useDispatch, useSelector } from "react-redux";
+import { IconChevronRight } from "@tabler/icons";
+import navigation from "./components/Admin/menu-items";
+import { drawerWidth } from "./store/constant";
+import { SET_MENU } from "./store/actions";
+import { Container } from "@mui/system";
+import { Sales } from "./Sales";
+import { TrafficByDevice } from "./TrafficByDevice";
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
+import Counters from "./components/Home/counters";
+import { SpecicesCounter } from "./components/SpeciesCounter";
+import { ImagesCounter } from "./components/ImagesCounter";
+import { RequestCounter } from "./components/RequestCounter";
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
     ...theme.typography.mainContent,
     ...(!open && {
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        }),
-        [theme.breakpoints.up('md')]: {
-            marginLeft: -(drawerWidth - 20),
-            width: `calc(100% - ${drawerWidth}px)`
-        },
-        [theme.breakpoints.down('md')]: {
-            marginLeft: '20px',
-            width: `calc(100% - ${drawerWidth}px)`,
-            padding: '16px'
-        },
-        [theme.breakpoints.down('sm')]: {
-            marginLeft: '10px',
-            width: `calc(100% - ${drawerWidth}px)`,
-            padding: '16px',
-            marginRight: '10px'
-        }
+      borderBottomLeftRadius: 0,
+      borderBottomRightRadius: 0,
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      [theme.breakpoints.up("md")]: {
+        marginLeft: -(drawerWidth - 20),
+        width: `calc(100% - ${drawerWidth}px)`,
+      },
+      [theme.breakpoints.down("md")]: {
+        marginLeft: "20px",
+        width: `calc(100% - ${drawerWidth}px)`,
+        padding: "16px",
+      },
+      [theme.breakpoints.down("sm")]: {
+        marginLeft: "10px",
+        width: `calc(100% - ${drawerWidth}px)`,
+        padding: "16px",
+        marginRight: "10px",
+      },
     }),
     ...(open && {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen
-        }),
-        marginLeft: 0,
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
-        width: `calc(100% - ${drawerWidth}px)`,
-        [theme.breakpoints.down('md')]: {
-            marginLeft: '20px'
-        },
-        [theme.breakpoints.down('sm')]: {
-            marginLeft: '10px'
-        }
-    })
-}));
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+      borderBottomLeftRadius: 0,
+      borderBottomRightRadius: 0,
+      width: `calc(100% - ${drawerWidth}px)`,
+      [theme.breakpoints.down("md")]: {
+        marginLeft: "20px",
+      },
+      [theme.breakpoints.down("sm")]: {
+        marginLeft: "10px",
+      },
+    }),
+  })
+);
 export default function Home() {
-    const theme = useTheme();
-    const matchDownMd = useMediaQuery(theme.breakpoints.down('lg'));
+  const theme = useTheme();
+  const matchDownMd = useMediaQuery(theme.breakpoints.down("lg"));
 
-    // Handle left drawer
-    const leftDrawerOpened = useSelector((state) => state.customization.opened);
-    const dispatch = useDispatch();
-    const handleLeftDrawerToggle = () => {
-        dispatch({ type: SET_MENU, opened: !leftDrawerOpened });
-    };
+  // Handle left drawer
+  const leftDrawerOpened = useSelector((state) => state.customization.opened);
+  const dispatch = useDispatch();
+  const handleLeftDrawerToggle = () => {
+    dispatch({ type: SET_MENU, opened: !leftDrawerOpened });
+  };
 
-    useEffect(() => {
-        dispatch({ type: SET_MENU, opened: !matchDownMd });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [matchDownMd]);
+  useEffect(() => {
+    dispatch({ type: SET_MENU, opened: !matchDownMd });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [matchDownMd]);
   return (
-    <Box sx={{ display: 'flex' }}>
-    <CssBaseline />
-    {/* header */}
-    <AppBar
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      {/* header */}
+      <AppBar
         enableColorOnDark
         position="fixed"
         color="inherit"
         elevation={0}
         sx={{
-            bgcolor: theme.palette.background.default,
-            transition: leftDrawerOpened ? theme.transitions.create('width') : 'none'
+          bgcolor: theme.palette.background.default,
+          transition: leftDrawerOpened
+            ? theme.transitions.create("width")
+            : "none",
         }}
-    >
+      >
         <Toolbar>
-            <Header handleLeftDrawerToggle={handleLeftDrawerToggle} />
+          <Header handleLeftDrawerToggle={handleLeftDrawerToggle} />
         </Toolbar>
-    </AppBar>
+      </AppBar>
 
-    {/* drawer */}
-    <Sidebar drawerOpen={leftDrawerOpened} drawerToggle={handleLeftDrawerToggle} />
+      {/* drawer */}
+      <Sidebar
+        drawerOpen={leftDrawerOpened}
+        drawerToggle={handleLeftDrawerToggle}
+      />
 
-    {/* main content */}
-    <Main theme={theme} open={leftDrawerOpened}>
+      {/* main content */}
+      <Main theme={theme} open={leftDrawerOpened}>
         {/* breadcrumb */}
-        <Breadcrumbs separator={IconChevronRight} navigation={navigation} icon title rightAlign />
-
-    </Main>
-</Box>
+        <Breadcrumbs
+          separator={IconChevronRight}
+          navigation={navigation}
+          icon
+          title
+          rightAlign
+        />
+        <div sx={{ height: "100%" }}>
+          <Box component="section"  >
+            <Box
+              component="main"
+              sx={{
+                flexGrow: 1,
+                py: 8,
+              }}
+            >
+              <Container maxWidth={false}>
+                <Grid container spacing={3}>
+               
+                  {/* <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <TotalProfit sx={{ height: '100%' }} />
+          </Grid> */}
+                  <Grid item lg={12} md={12} xl={12} xs={12} >
+                    <Sales />
+                  </Grid>
+                  {/* <Grid item lg={4} md={6} xl={3} xs={12}>
+                    <TrafficByDevice sx={{ height: "100%" }} />
+                  </Grid> */}
+                     <Grid
+          
+          item
+          lg={4}
+          sm={6}
+          xl={4}
+          xs={12}
+        >
+          <SpecicesCounter  className={styles.dashboard}/>
+          {/* <Counters /> */}
+        </Grid>
+                <Grid
+          item
+          xl={4}
+          lg={4}
+          sm={6}
+          xs={12}
+        >
+          <ImagesCounter className={styles.dashboard}/>
+        </Grid>
+                <Grid
+          item
+          xl={4}
+          lg={4}
+          sm={6}
+          xs={12}
+        >
+          <RequestCounter className={styles.dashboard} />
+        </Grid>
+                </Grid>
+              </Container>
+            </Box>
+          </Box>
+        </div>
+      </Main>
+    </Box>
   );
 }
