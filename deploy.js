eccef5221c84e9e8ed660dbcd950078141c10e0f
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2022-09-12 17:14:58 +0600

    make asyncronus

--- deploy.js
+++ deploy.js
@@ -1,25 +1,22 @@
 const shell = require('shelljs');
 
 
-const nextBuild = () => {
-    let res = shell.exec('next build');
+const deploy = async () => {
+    let res = await shell.exec('next build');
     if (res.code !== 0) {
       shell.echo('Error: next build failed');
       shell.exit(1);
     }
-    let res2 = shell.exec('next export');
+    let res2 = await shell.exec('next export');
     if (res2.code !== 0) {
       shell.echo('Error: next build failed');
       shell.exit(1);
     }
-  }
-  const deploy = () => {
-    let res = shell.exec('gh-pages -d out');
-    if (res.code !== 0) {
+    let res3 = await shell.exec('gh-pages -d out');
+    if (res3.code !== 0) {
       shell.echo('Error: deploy build failed');
       shell.exit(1);
     }
   }
 
-nextBuild()
 deploy()
