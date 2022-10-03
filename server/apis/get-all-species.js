3547bb95743280c814c6503740c80b8709b3f40a
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2022-10-03 21:27:55 +0600

    updated species and data

--- server/apis/get-all-species.js
+++ server/apis/get-all-species.js
@@ -4,11 +4,10 @@ const DB = require("../config/connectToDatabase");
 
 exports.getAllSpecies = async (req, res, next) => {
     let modifiedList = []
-    Object.keys(speciesTableTypes).map(async (key, index) => {
+    for (let key of Object.keys(speciesTableTypes)) {
         let table = await getTable(speciesTableTypes[key])
         let searchQuery = `select * from ${table} where marker is not null`
         let response = await executeQuery(searchQuery)
-        // console.log({ response })
         if (response?.length > 0) {
             let modifiedResponse = []
             for (let item of response) {
@@ -18,22 +17,17 @@ exports.getAllSpecies = async (req, res, next) => {
                     additionalFiles: item?.additionaL_files?.split(',') || '',
                     name: item?.name ? JSON.parse(item.name) : {},
                 })
-                if(response.length == modifiedResponse.length){
-                    modifiedList = modifiedList.concat(modifiedResponse)
-                }
             }
-            if (Object.keys(speciesTableTypes).length - 1 == index) {
-                console.log(modifiedList.length)
-
-                res.status(200).json({
-                    success: true,
-                    data: modifiedList,
-                })
-            }
+            modifiedList = modifiedList.concat(modifiedResponse)
         }
+    }
 
-
+    console.log(modifiedList.length)
+    res.status(200).json({
+        success: true,
+        data: modifiedList,
     })
+
 
 
 }
