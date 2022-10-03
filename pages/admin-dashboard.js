d46bc8059b8ad44cb098f37bf0ac6800e48861da
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2022-10-03 17:04:54 +0600

    updated admin dashboard

--- pages/admin-dashboard.js
+++ pages/admin-dashboard.js
@@ -96,11 +96,9 @@ export default function Home() {
         position="fixed"
         color="inherit"
         elevation={0}
-        style={}
         sx={{
           bgcolor: theme.palette.background.default,
           boxshadow: '1px 1px 10px #d9d5d5 !important',
-
           transition: leftDrawerOpened
             ? theme.transitions.create("width")
             : "none",
