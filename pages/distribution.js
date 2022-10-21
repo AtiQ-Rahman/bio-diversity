4f193ceb96a5a6cbe3df32fd339e78fe3366cbfc
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2022-10-21 12:07:59 +0600

    Updated distribution

--- pages/distribution.js
+++ pages/distribution.js
@@ -144,137 +144,123 @@ const Distribution = () => {
     }, [modifiedList]);
     return (
 
-
-        <Box
-            component="main"
-            sx={{
-                flexGrow: 1,
-
+        <Grid
+            container
+            style={{
+                width: "100%",
+                height: "100vh",
+                overflow: "auto"
             }}
         >
             <Grid
-                container
-                spacing={3}
-                xs={12}
+                item
+                style={{
+                    width: "100%",
+                    height: "100%"
+                }}
                 md={12}
-
+                xl={12}
+                xs={12}
             >
-                <Grid
-                    item
-                    md={12}
-                    xl={12}
-                    xs={12}
-                    style={{ borderRadius: "10px" }}
-                // style={{  paddingRight: "20px" }}
-                >
-                    <div className={styles.sidebar}>
-                        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-                    </div>
-                    <Box className={styles.details_bar}>
+                <div className={styles.sidebar}>
+                    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                </div>
+                <Box className={styles.details_bar}>
 
-                        <Card sx={{ maxWidth: 345, height: 1080 }}>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Total Species {modifiedList.length}
-                                </Typography>
+                    <Card sx={{ maxWidth: 345, height: "100%" }}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Total Species {modifiedList.length}
+                            </Typography>
 
-                                <Grid item xs={12}>
-                                    <TextField
+                            <Grid item xs={12}>
+                                <TextField
 
-                                        id="Species"
-                                        name="name"
-                                        margin="normal"
-                                        size="small"
-                                        label="Search By English Name"
-                                        fullWidth
-                                        onChange={(e) => {
-                                            let modifiedList = speciesList.filter((species) => {
-                                                let value = e.target.value.toLocaleLowerCase()
-                                                // if(species?.name?.commonName.toLocaleLowerCase().includes(value) 
-                                                // || species?.name?.bangla.toLocaleLowerCase().includes(value)
-                                                // || species?.name?.english.toLocaleLowerCase().includes(value)
-                                                // || species?.name?.synonym.toLocaleLowerCase().includes(value)) {
-                                                //     return species
-                                                // }
-                                                if (species?.english.toLocaleLowerCase().includes(value)) {
-                                                    return species
-                                                }
-                                            })
-                                            setModifiedList(modifiedList)
-                                            console.log({ modifiedList })
+                                    id="Species"
+                                    name="name"
+                                    margin="normal"
+                                    size="small"
+                                    label="Search By English Name"
+                                    fullWidth
+                                    onChange={(e) => {
+                                        let modifiedList = speciesList.filter((species) => {
+                                            let value = e.target.value.toLocaleLowerCase()
+                                            // if(species?.name?.commonName.toLocaleLowerCase().includes(value) 
+                                            // || species?.name?.bangla.toLocaleLowerCase().includes(value)
+                                            // || species?.name?.english.toLocaleLowerCase().includes(value)
+                                            // || species?.name?.synonym.toLocaleLowerCase().includes(value)) {
+                                            //     return species
+                                            // }
+                                            if (species?.english.toLocaleLowerCase().includes(value)) {
+                                                return species
+                                            }
+                                        })
+                                        setModifiedList(modifiedList)
+                                        console.log({ modifiedList })
 
-                                        }}
-                                        autoComplete="Search By Common Name"
-                                        variant="outlined"
-                                    />
-                                </Grid>
-                                {modifiedList.length > 0 ? (
-                                    <TableContainer component={Paper} sx={{ maxHeight: 440 }}  >
-                                        <Table sx={{ maxWidth: 340 }} aria-label="customized table" >
-                                            <TableBody   >
-                                                {modifiedList.map((species, index) => (
-                                                    <>
-                                                        <TableRow
-                                                            key={index}
-                                                            sx={{
-                                                                "&:last-child td, &:last-child th": { border: 0 },
-                                                            }}
-                                                        >
-                                                            <TableCell component="td" scope="row" width={50}>
-                                                                <Image height={50} width={40} src={species.marker}></Image>
-                                                            </TableCell>
-                                                            <TableCell align="">
-                                                                <Typography variant="body2" color="text.primary">
-                                                                    {species.english}
-                                                                </Typography>
+                                    }}
+                                    autoComplete="Search By Common Name"
+                                    variant="outlined"
+                                />
+                            </Grid>
+                            {modifiedList.length > 0 ? (
+                                <TableContainer component={Paper} sx={{ maxHeight: 440 }}  >
+                                    <Table sx={{ maxWidth: 340 }} aria-label="customized table" >
+                                        <TableBody   >
+                                            {modifiedList.map((species, index) => (
+                                                <>
+                                                    <TableRow
+                                                        key={index}
+                                                        sx={{
+                                                            "&:last-child td, &:last-child th": { border: 0 },
+                                                        }}
+                                                    >
+                                                        <TableCell component="td" scope="row" width={50}>
+                                                            <Image height={50} width={40} src={species.marker}></Image>
+                                                        </TableCell>
+                                                        <TableCell align="">
+                                                            <Typography variant="body2" color="text.primary">
+                                                                {species.english}
+                                                            </Typography>
 
-                                                            </TableCell>
-                                                            <TableCell>
-                                                                <Typography variant="caption">
-                                                                    {twoDecimal(species.districts[0].center[0])} ,{twoDecimal(species.districts[0].center[1])}
-                                                                </Typography>
-                                                            </TableCell>
-                                                        </TableRow>
+                                                        </TableCell>
+                                                        <TableCell>
+                                                            <Typography variant="caption">
+                                                                {twoDecimal(species.districts[0].center[0])} ,{twoDecimal(species.districts[0].center[1])}
+                                                            </Typography>
+                                                        </TableCell>
+                                                    </TableRow>
 
-                                                    </>
+                                                </>
 
-                                                ))}
-                                            </TableBody>
-                                        </Table>
-                                    </TableContainer>
-                                ) : null}
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            ) : null}
 
-                                <Typography variant="body2" color="text.secondary">
-                                    The full name of the genus or species can be inserted, or
-                                    you can type the first four letters of the generic name
-                                    and/or the first four letters of the species (or other)
-                                    epithet in upper or lower case (e.g. Mere micr or mere micr
-                                    for Meredithia microphylla). A full list of the species and
-                                    subspecific entities in each genus can be obtained in the
-                                    genus database.
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small">Share</Button>
-                                <Button size="small">Learn More</Button>
-                            </CardActions>
-                        </Card>
+                            <Typography variant="body2" color="text.secondary">
+                                The full name of the genus or species can be inserted, or
+                                you can type the first four letters of the generic name
+                                and/or the first four letters of the species (or other)
+                                epithet in upper or lower case (e.g. Mere micr or mere micr
+                                for Meredithia microphylla). A full list of the species and
+                                subspecific entities in each genus can be obtained in the
+                                genus database.
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Share</Button>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
 
-                    </Box>
-                    <div ref={mapContainer} className={styles.map_container}></div>
-
-                </Grid>
+                </Box>
+                <div ref={mapContainer} className={styles.map_container}></div>
 
             </Grid>
 
-            {/*  */}
-        </Box>
-
-
-
-
-
-
+        </Grid>
     );
 };
 Distribution.getInitialProps = ({ query }) => {
