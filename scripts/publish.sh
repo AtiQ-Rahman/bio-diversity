58dc1b878e38c1349aed15c15bab50e5f95e32f1
Rahat Saqib <rahatsaqib78@gmail.com>
2022-12-13 10:48:44 +0600

    updated scripts

---
+++ scripts/publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Build variables.
+export ENVIRONMENT=production
+
+# Deployment variables.
+export SERVER_HOST=66.29.151.71
+export SERVER_DIR=bio-diversity
+export SERVER_USER=bio
+
+
+# Build app.
+echo "Building the production build for the app...."
+    npm run build --scripts-prepend-node-path &&
+    echo "Production build for the app built!" &&
+    echo "Uploading files to web server..." &&
+    echo 'put -r .next/*' | sftp $SERVER_USER@$SERVER_HOST:$SERVER_DIR &&
+    echo "Uploaded files to web server!"    
