ea5fd7588fa35e866462c8eee61ed24b65e164db
Rahat Saqib <rahatsaqib78@gmail.com>
2022-12-13 17:47:58 +0600

    Updated connecttodatabase

--- server/config/connectToDatabase.js
+++ server/config/connectToDatabase.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config()
 const DB = mysql.createPool({
     host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     multipleStatements: true
 });
 
