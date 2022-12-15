7f995947a65dd4f5ce39a76f7f2b469201e5d87e
Rahat Saqib <rahatsaqib78@gmail.com>
2022-12-15 19:53:00 +0600

    Updated next config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withSass = require('@zeit/next-sass');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  distDir: "build",
   env: {
     mapbox_key: "pk.eyJ1IjoiaC10ZWNoIiwiYSI6ImNsN3AxMDR6MzBwOHYzb25zNnl3Zjc4eHAifQ.uSN0ORQ4zG2ubae--AAsjw",
     mapStyle: "mapbox://styles/h-tech/cl8szmvbz001914qgmr0zt27j",
