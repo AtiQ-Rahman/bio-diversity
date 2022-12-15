791733591dc0a3d3e5084ddc34b32b0e925bb928
Rahat Saqib <rahatsaqib78@gmail.com>
2022-12-15 20:15:04 +0600

    Updated all script for optimized deployments

--- server/deploy.js
+++ server/deploy.js
@@ -2,5 +2,6 @@
 
 const fetch = require("node-fetch");
 let url = `http://66.29.151.71:8080/update-LEQpNz2GB6PRh5uM`
-let response = fetch(url, {method: 'POST', body: JSON.stringify({})})
-console.log({response})
+fetch(url, {method: 'POST', body: JSON.stringify({})}).then((res)=>{
+  console.log({res})
+})
