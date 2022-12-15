ca22ce87eabeda8c901dae34dc8455d4f88c9d68
Rahat Saqib <rahatsaqib78@gmail.com>
2022-12-15 19:47:28 +0600

    updated automatic updater

---
+++ scripts/updater.js
@@ -0,0 +1,43 @@
+
+const PORT = 8080;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
+const shell = require('shelljs');
+
+
+
+app.post('/update-LEQpNz2GB6PRh5uM', async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write("Started Upload Process\n");
+  let results;
+
+  results = shell.exec('git pull');
+  res.write(String(results.code));
+  res.write(String(results.stdout));
+  res.write(String(results.stderr));
+  res.write('\n\n');
+
+  results = shell.exec('pm2 restart bio_client --watch');
+  res.write(String(results.code));
+  res.write(String(results.stdout));
+  res.write(String(results.stderr));
+  res.write('\n\n');
+
+  results = shell.exec('pm2 show bio_client');
+  res.write(String(results.code));
+  res.write(String(results.stdout));
+  res.write(String(results.stderr));
+  res.write('\n\n');
+
+  res.end('Finished');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
