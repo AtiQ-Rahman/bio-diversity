29edb562b307abde5eb4752c8e295c7d66b45444
Rahat Saqib <rahatsaqib78@gmail.com>
2022-12-20 23:39:12 +0600

    Updated deploy script

--- server/deploy.js
+++ server/deploy.js
@@ -2,6 +2,4 @@
 
 const fetch = require("node-fetch");
 let url = `http://66.29.151.71:8080/update-LEQpNz2GB6PRh5uM`
-fetch(url, {method: 'POST', body: JSON.stringify({})}).then((res)=>{
-  console.log({res})
-})
+fetch(url, {method: 'POST', body: JSON.stringify({})})
